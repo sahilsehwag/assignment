018149424be0d6debd644523c60047d823afac22
sahil sehwag <sehwagsahil002@gmail.com>
2020-09-24 20:32:41 +0530

    Added some build commands

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static('client/build'))
+}
